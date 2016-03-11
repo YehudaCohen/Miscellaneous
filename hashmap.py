2be5ea75f70de2190a0377b6af70856aafecc8ad
YehudaCohen <yehudac@gmail.com>
2016-03-10 23:08:59 -0500

    naive, pretty, short hashmap implementation in python with a couple of test cases

---
+++ hashmap.py
@@ -0,0 +1,23 @@
+class HashMap:
+
+    def __init__(self,n_buckets, hash_function_generator):
+        self.n_buckets = n_buckets
+        self.array = n_buckets * [None]
+        self.hash = hash_function_generator(n_buckets)
+
+
+    def set(self, key, value):
+        self.array.insert(self.hash(key), value)
+
+    def get(self, key):
+        return self.array[self.hash(key)]
+
+
+def hash_function_generator(n_buckets):
+    return lambda input: sum((ord(char) for char in str(input))) % n_buckets
+
+a = HashMap(100, hash_function_generator)
+a.set("hello", True)
+
+assert a.get("hello")
+assert a.get("goodbye") == False
